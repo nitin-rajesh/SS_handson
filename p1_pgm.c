95de1d7cf3267c81654ed6b306fbe90c2ea64ad8
nitin-rajesh <nitinkumar.rajesh@iiitb.ac.in>
2024-08-22 09:36:27 +0530

    P1 pgm update

--- p1_pgm.c
+++ p1_pgm.c
@@ -1,6 +1,9 @@
 /* Name: Nitin Kumar Rajesh
  * Reg no: MT2024103
- * Problem statement: 
+ * Problem 1: Create the following types of a files using (i) shell command (ii) system call
+    a. soft link (symlink system call)
+    b. hard link (link system call)
+    c. FIFO (mkfifo Library Function or mknod system call)
 */
 
 #include<stdio.h>
